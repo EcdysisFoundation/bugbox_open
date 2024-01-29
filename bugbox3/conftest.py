3e381f77f042d00de7b112b11892095e8c9884ad
Mikayla Wilson <mikaylaelectra@gmail.com>
2024-01-29 16:59:17 -0500

    initial cookiecutter commit

---
+++ bugbox3/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bugbox3.users.models import User
+from bugbox3.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
