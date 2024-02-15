db719698e84d0a3715b44f7c319cad5464869145
Mikayla Wilson <mikaylaelectra@gmail.com>
2024-02-15 09:37:21 -0500

    logo, about and mybox navigation

--- config/urls.py
+++ config/urls.py
@@ -6,8 +6,9 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path("", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
+    path("my-bugbox", TemplateView.as_view(template_name="pages/my-bugbox.html"), name="my-bugbox"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
