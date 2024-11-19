b60d06d153c580c56eb8226e0d32d724a5177e1b
Mikayla Wilson <mikaylaelectra@gmail.com>
2024-11-19 13:18:33 -0500

    check which platform first

--- bugbox3/core/management/commands/classify_new_images.py
+++ bugbox3/core/management/commands/classify_new_images.py
@@ -1,4 +1,5 @@
 from django.apps import apps
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db.models import Exists, OuterRef
 
@@ -11,17 +12,18 @@ class Command(BaseCommand):
     SpecimenImage = apps.get_model(app_label='samples', model_name='SpecimenImage')
 
     def handle(self, *args, **options):
-        specimens = self.Specimen.objects.annotate(
-            has_images=Exists(self.SpecimenImage.objects.filter(
-                specimen=OuterRef('pk'),
-                image_notfound=False))).filter(
-            ai_classification__isnull=True,
-            acceptance=0,
-            has_images=True
-        )
-        print(len(specimens))
-        for s in specimens:
-            print(s.date_added)
-            print('')
-        for s in specimens:
-            id_image.delay(s.id)
+        if settings.AI_INFERENCE_URL:
+            specimens = self.Specimen.objects.annotate(
+                has_images=Exists(self.SpecimenImage.objects.filter(
+                    specimen=OuterRef('pk'),
+                    image_notfound=False))).filter(
+                ai_classification__isnull=True,
+                acceptance=0,
+                has_images=True
+            )
+            for s in specimens:
+                id_image.delay(s.id)
+        else:
+            print(
+                'AI_INFERENCE_URL is unavailable, classify_new_images command not run.'
+            )
