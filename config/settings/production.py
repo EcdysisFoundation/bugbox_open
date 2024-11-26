8a2dc6130f74b13e8d35bbda4bec5d9a176adfdf
Mikayla Wilson <mikaylaelectra@gmail.com>
2024-11-26 08:50:40 -0500

    add subdomain

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,8 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "bugbox-381407baad84.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "bugbox-381407baad84.herokuapp.com",
+                 "bugbox.ecdysis.bio"]
 
 # DATABASES
 # ------------------------------------------------------------------------------
