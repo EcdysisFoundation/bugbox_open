ec0fee4ea620adcfcb194eec4b599369362025bc
Mikayla Wilson <mikaylaelectra@gmail.com>
2025-02-13 09:39:20 -0500

    add name to signup form as required field

--- bugbox3/users/forms.py
+++ bugbox3/users/forms.py
@@ -2,6 +2,7 @@ from allauth.account.forms import SignupForm
 from allauth.socialaccount.forms import SignupForm as SocialSignupForm
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
+from django.forms import CharField
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
@@ -31,6 +32,7 @@ class UserSignupForm(SignupForm):
     Default fields will be added automatically.
     Check UserSocialSignupForm for accounts created from social.
     """
+    name = CharField(max_length=255, label='Name of User')
 
 
 class UserSocialSignupForm(SocialSignupForm):
