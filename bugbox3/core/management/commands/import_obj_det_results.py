2b41261d7d1840e03911d81c0141291d6738a699
Mikayla Wilson <mikaylaelectra@gmail.com>
2025-04-16 14:15:30 -0400

    cmd to save json export to specimenimage recs

---
+++ bugbox3/core/management/commands/import_obj_det_results.py
@@ -0,0 +1,49 @@
+import json
+from datetime import datetime, timezone
+
+from django.apps import apps
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    SpecimenImage = apps.get_model(
+        app_label='samples', model_name='SpecimenImage')
+
+    def handle(self, *args, **kwargs):
+
+        if settings.ON_ECDYSIS_SERVER != 'YES':
+            print('Currently this cmd is only supported on Ecdysis01')
+            return
+
+        csv_path = 'local_files/obj_det_export.json'
+
+        with open(csv_path, 'r') as file:
+            data = json.load(file)
+            saved = 0
+            skipped = 0
+            for d in data:
+                si = self.SpecimenImage.objects.get(
+                    id=d['meta']['id'], specimen_id=d['meta']['specimen_id'])
+                if si.object_det_updated_at:
+                    thedate = datetime.fromisoformat(d['updated_at'][:-1]).astimezone(timezone.utc)
+                    if si.object_det_updated_at == thedate:
+                        skipped += 1
+                        continue
+                si.object_det_label=d['label']
+                si.object_det_annotation_id=d['annotation_id']
+                si.object_det_id=d['id']
+                si.object_det_updated_at=d['updated_at']
+                si.save()
+                saved += 1
+
+            print('Completed, saved {0} new annotations, skipped {1}'.format(
+                saved, skipped
+            ))
+
+
+
+
+
+
