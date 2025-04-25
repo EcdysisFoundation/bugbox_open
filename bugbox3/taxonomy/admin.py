e39331bc9a6ff719e0d0855ab3be0322a32f4e93
diaanassar <43388125+diaanassar@users.noreply.github.com>
2025-04-25 15:15:34 -0500

    includingMigrations

--- bugbox3/taxonomy/admin.py
+++ bugbox3/taxonomy/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
 from .models import GBIFImageRecord
 
-admin.site.register(GBIFImageRecord)
+@admin.register(GBIFImageRecord)
+class GBIFImageRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'gbif_taxon_key',
+        'scientific_name',
+        'image_url',
+        'media_type',
+        'license',
+        'morphospecies',
+        'date_fetched',
+        'decimal_latitude',
+        'decimal_longitude',
+        'occurrence_id',
+        'downloaded_image',
+    )
+    search_fields = ('scientific_name', 'gbif_taxon_key', 'occurrence_id')
+    list_filter = ('downloaded_image', 'media_type')
