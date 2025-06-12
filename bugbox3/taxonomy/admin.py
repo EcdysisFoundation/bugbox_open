b53ae05a0b57b57aeb60578753e5b415a551c942
diaanassar <43388125+diaanassar@users.noreply.github.com>
2025-06-12 12:56:47 -0500

    merge main into bigfetchgbif

--- bugbox3/taxonomy/admin.py
+++ bugbox3/taxonomy/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GBIFImageRecord
+from .models import GBIFImageRecord, FilteredGBIFImageRecord
 
 @admin.register(GBIFImageRecord)
 class GBIFImageRecordAdmin(admin.ModelAdmin):
@@ -18,4 +18,24 @@ class GBIFImageRecordAdmin(admin.ModelAdmin):
         'downloaded_image',
     )
     search_fields = ('scientific_name', 'gbif_taxon_key', 'occurrence_id')
+    list_filter = ('downloaded_image', 'media_type')
+
+
+@admin.register(FilteredGBIFImageRecord)
+class FilteredGBIFImageRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'gbif_taxon_key',
+        'canonical_name',
+        'image_url',
+        'media_type',
+        'license',
+        'morphospecies',
+        'date_fetched',
+        'decimal_latitude',
+        'decimal_longitude',
+        'occurrence_id',
+        'downloaded_image',
+    )
+    search_fields = ('canonical_name', 'gbif_taxon_key', 'occurrence_id')
     list_filter = ('downloaded_image', 'media_type')
