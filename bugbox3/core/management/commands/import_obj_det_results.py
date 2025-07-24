9da65c7fc4efd12719a794bdbb35a46891e7a21e
Mikayla Wilson <mikaylaelectra@gmail.com>
2025-07-24 11:03:49 -0400

    handle unlabeled images
    :

--- bugbox3/core/management/commands/import_obj_det_results.py
+++ bugbox3/core/management/commands/import_obj_det_results.py
@@ -17,27 +17,32 @@ class Command(BaseCommand):
             print('Currently this cmd is only supported on Ecdysis01')
             return
 
-        csv_path = 'local_files/obj_det_export.json'
+        json_path = 'local_files/obj_det_export1.json'
 
-        with open(csv_path, 'r') as file:
+        with open(json_path, 'r') as file:
             data = json.load(file)
             saved = 0
             skipped = 0
-            for d in data:
-                si = self.SpecimenImage.objects.get(
-                    id=d['meta']['id'], specimen_id=d['meta']['specimen_id'])
-                if si.object_det_updated_at:
-                    thedate = datetime.fromisoformat(d['updated_at'][:-1]).astimezone(timezone.utc)
-                    if si.object_det_updated_at == thedate:
-                        skipped += 1
-                        continue
-                si.object_det_label = d['label']
-                si.object_det_annotation_id = d['annotation_id']
-                si.object_det_id = d['id']
-                si.object_det_updated_at = d['updated_at']
-                si.object_det_model_version = ''
-                si.save()
-                saved += 1
+            for i, d in enumerate(data):
+                if 'label' in d.keys():
+                    si = self.SpecimenImage.objects.get(
+                        id=d['meta']['id'], specimen_id=d['meta']['specimen_id'])
+                    if si.object_det_updated_at:
+                        thedate = datetime.fromisoformat(d['updated_at'][:-1]).astimezone(timezone.utc)
+                        if si.object_det_updated_at == thedate:
+                            print(f'Record number {i} was already saved with this timestamp skipping')
+                            skipped += 1
+                            continue
+                    si.object_det_label = d['label']
+                    si.object_det_annotation_id = d['annotation_id']
+                    si.object_det_id = d['id']
+                    si.object_det_updated_at = d['updated_at']
+                    si.object_det_model_version = ''
+                    si.save()
+                    saved += 1
+                else:
+                    print(f'No label in item number {i}, skipping')
+                    skipped += 1
 
             print('Completed, saved {0} new annotations, skipped {1}'.format(
                 saved, skipped
