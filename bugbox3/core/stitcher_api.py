315b2c13cf3b8def01263a8d34f1ccfbf75bbe3e
Mikayla Wilson <mikaylaelectra@gmail.com>
2025-08-28 10:28:59 -0400

    use approved

--- bugbox3/core/stitcher_api.py
+++ bugbox3/core/stitcher_api.py
@@ -3,10 +3,11 @@ import requests
 
 STITCHER_URL = 'http://host.docker.internal:8090'
 
+# move js_URLs dev differences to local.yml and local-cloud.yml
 STITCHER_JS_URL_ZEROTIER = 'http://10.147.19.124:8090'
 
 # for local dev
-#STITCHER_JS_URL_ZEROTIER = 'http://localhost:8090'
+# STITCHER_JS_URL_ZEROTIER = 'http://localhost:8090'
 
 STITCHER_JS_URL = 'http://ecdysis01.local:8090'
 
@@ -24,7 +25,7 @@ def list_upload_files():
         params = {
             'offset': offset,
             'limit': limit,
-            'label_studio_filter': True
+            'approved': True
         }
         print(params)
 
