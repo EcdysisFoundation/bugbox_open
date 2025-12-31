77cdd67d5b3a43570781356f885a6d0b06da533c
diaanassar <diaanasar8@gmail.com>
2025-12-31 10:04:58 -0600

    removing custom generation -no longer needed- , Moved the JS code used in label_management.html to the webpack bundle

--- bugbox3/grower_portal/views/admin/label_management.py
+++ bugbox3/grower_portal/views/admin/label_management.py
@@ -8,7 +8,7 @@ from django.views.decorators.http import require_http_methods
 
 from bugbox3.core.permissions import IS_GROWERADMIN
 from ...models import LabelGeneration
-from ...forms.admin.label_forms import LabelGenerationForm, QuickLabelGenerationForm
+from ...forms.admin.label_forms import QuickLabelGenerationForm
 from ...services.label_generator import LabelGenerator
 from ...middleware import get_user_timezone
 from ...constants import SAMPLE_TYPES
@@ -19,7 +19,7 @@ from ...constants import SAMPLE_TYPES
 def label_management(request):
     """Main label generation page"""
     if request.method == 'POST':
-        form_type = request.POST.get('form_type', 'detailed')
+        form_type = request.POST.get('form_type', 'quick')
         
         if form_type == 'quick':
             quick_form = QuickLabelGenerationForm(request.POST)
@@ -131,83 +131,16 @@ def label_management(request):
                 except Exception as e:
                     messages.error(request, f'Error generating quick labels: {str(e)}')
             
-            form = LabelGenerationForm()
             context = {
                 'quick_form': quick_form,
-                'form': form,
-                'avalanche_sample_types': SAMPLE_TYPES,
-                'thousand_farms_sample_types': SAMPLE_TYPES,
                 'user_timezone': get_user_timezone(request)
             }
             return render(request, 'grower_portal/admin/label_management.html', context)
-        
-        else:
-            form = LabelGenerationForm(request.POST)
-            
-            if form.is_valid():
-                project_type = form.cleaned_data['project_type']
-                label_category = form.cleaned_data['label_category']
-                
-                if label_category == 'outer':
-                    messages.info(request, 'Outer labels to be implemented....')
-                    return redirect('grower_portal:label_management')
-                
-                # inner labels
-                cluster_number = form.cleaned_data['cluster_number']
-                year = form.cleaned_data['year']
-                sample_types = form.cleaned_data['sample_types']
-                labels_per_type = form.cleaned_data['labels_per_type']
-                description = form.cleaned_data.get('description', '')
-                
-                try:
-                    generator = LabelGenerator(
-                        project_type=project_type,
-                        cluster_number=cluster_number,
-                        year=year,
-                        sample_types=sample_types,
-                        labels_per_type=labels_per_type,
-                        created_by=request.user
-                    )
-                    
-                    buffer, total_labels = generator.generate_document_with_table()
-                    
-                    # Save to model
-                    filename = f"labels_{project_type}_{cluster_number}_{year}.docx"
-                    label_generation = LabelGeneration.objects.create(
-                        project_type=project_type,
-                        label_category=label_category,
-                        cluster_number=cluster_number,
-                        year=year,
-                        sample_types=sample_types,
-                        labels_per_type=labels_per_type,
-                        total_labels_generated=total_labels,
-                        generated_by=request.user,
-                        description=description,
-                        transect_codes_generated=generator.generated_codes
-                    )
-                    
-                    # Save file
-                    label_generation.label_file.save(filename, ContentFile(buffer.read()), save=True)
-                    
-                    messages.success(
-                        request,
-                        f'Successfully generated {total_labels} labels! '
-                        f'Generated {len(generator.generated_codes)} unique transect codes.'
-                    )
-                    
-                    return redirect('grower_portal:label_generation_detail', generation_id=label_generation.id)
-                    
-                except Exception as e:
-                    messages.error(request, f'Error generating labels: {str(e)}')
     else:
-        form = LabelGenerationForm()
         quick_form = QuickLabelGenerationForm()
     
     context = {
-        'form': form,
         'quick_form': quick_form,
-        'avalanche_sample_types': SAMPLE_TYPES,
-        'thousand_farms_sample_types': SAMPLE_TYPES,
         'user_timezone': get_user_timezone(request)
     }
     
