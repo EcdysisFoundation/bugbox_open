a160e4e036aa86355e07d2a3bf8d51bf741a0bb5
Mikayla Wilson <mikaylaelectra@gmail.com>
2026-01-29 16:21:20 -0500

    local-cloud.yml uses gunicorn nginx production servers

--- webpack/prodlocal.config.js
+++ webpack/prodlocal.config.js
@@ -1,8 +1,9 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./common.config');
 
-// This variable should mirror the one from config/settings/production.py
-const staticUrl = `https://ecdysis-static.s3.amazonaws.com/`;
+
+// This variable should mirror the one from config/settings/localproduction.py
+const staticUrl = '/static/';
 
 module.exports = merge(commonConfig, {
   mode: 'production',
