ff3d88fd547df30b65c145a8efe193b1ae4e6340
Mikayla Wilson <mikaylaelectra@gmail.com>
2026-03-06 07:49:38 -0500

    use local domain on linux

--- bugbox3/core/stitcher_api.py
+++ bugbox3/core/stitcher_api.py
@@ -4,7 +4,7 @@ import re
 import requests
 from requests.exceptions import Timeout
 
-STITCHER_URL = 'http://host.docker.internal:8090'
+STITCHER_URL = 'http://ecdysis01.local:8090'
 
 # to access production remotely
 # STITCHER_URL = 'http://10.147.19.124:8090'
