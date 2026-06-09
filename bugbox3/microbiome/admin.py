7ec5d895e60383be0545df2b498d7a91b3451db6
Mikayla Wilson <mikaylaelectra@gmail.com>
2026-06-09 11:12:46 -0400

    update fields

--- bugbox3/microbiome/admin.py
+++ bugbox3/microbiome/admin.py
@@ -6,7 +6,7 @@ from .models import MicrobiomeTaxa
 class MicrobiomeTaxaAdmin(ModelAdmin):
     search_fields = ('file',)
     ordering = ['file']
-    list_display = ['lab_analytics_source', 'sample_year', 'target_region' 'file', 'date_added']
+    list_display = ['lab_analytics_source', 'target_region', 'file', 'date_added']
 
 
 site.register(MicrobiomeTaxa, MicrobiomeTaxaAdmin)
